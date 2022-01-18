04781ad122062ff205b4fa2d0566211d73b34627
zamadtahir <zamad.tahir77@gmail.com>
2022-01-18 19:14:26 +0500

    layout structure fixes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-import { useEffect, useState } from 'react';
-import './App.scss';
-import Home from './pages/home'
-import ProductPage from './pages/productpage'
-import SignIn from './pages/signin'
-import SignUp from './pages/signup'
-import Scroll from './components/Scroll'
+import { useEffect, useState } from "react";
+import "./App.scss";
+import Home from "./pages/home";
+import ProductPage from "./pages/productpage";
+import SignIn from "./pages/signin";
+import SignUp from "./pages/signup";
+import Scroll from "./components/Scroll";
 import { Routes, Route, Link } from "react-router-dom";
-import WishList from './pages/wishlist/wishlist';
-import PrivacyPolicy from './pages/privacypolicy/privacypolicy';
+import WishList from "./pages/wishlist/wishlist";
+import PrivacyPolicy from "./pages/privacypolicy/privacypolicy";
+import Sidebar from "./components/sidebar";
+import Header from "./components/header";
+
+
 function App() {
-  const [currentProduct,setCurrentProduct]=useState("")
-  return(
+  const [currentProduct, setCurrentProduct] = useState("");
+  return (
     <>
-         <Scroll />
-       <Routes>
-        <Route path="/" element={<Home prod={currentProduct} currentProd={setCurrentProduct} />} />
-        <Route path="product" element={<ProductPage prod={currentProduct} currentProd={setCurrentProduct} />} />
-        <Route path="/signin" element={<SignIn  />} />
-        <Route path="/signup" element={<SignUp  />} />
-        <Route path="/wishlist" element={<WishList />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-      </Routes>
-    
-    
-
+      <div class="layout">
+        <Sidebar></Sidebar>
+        <div class="body-container">
+          <Header></Header>
+          <div class="body-content">
+            this is body
+            <Routes>
+              <Route path="/" element={<Home prod={currentProduct} currentProd={setCurrentProduct} />} />
+              <Route path="product" element={<ProductPage prod={currentProduct} currentProd={setCurrentProduct} />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/wishlist" element={<WishList />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </>
-  )
-};
-
+  );
+}
 
 export default App;
---
+++ src/components/sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Sidebar = () =>{
+    return(
+        <>
+        <div className='sidebar'>
+        this is sidebar
+        </div>
+        </>
+    )
+}
+export default Sidebar
